ec8a404141cc44fe0d0842fc09f296803f5f5049
49831117 <49831117@mail.nknu.edu.tw>
2021-03-04 21:28:56 +0800

    update

--- .py/housing_raw_data.py
+++ .py/housing_raw_data.py
@@ -1,12 +1,15 @@
+
+'''
+1. 指定查詢年分
+2. real_estate_crawler(year, season) 爬取資料並儲存、解壓縮
+3. 合併成一份 df
+4. 將 df 存成 .csv，檔名為 f"all_{start_y}_to_{end_y}.csv"
+'''
+
 import requests
 import os
 import zipfile
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import geopandas as gpd
-import geoplot as gplt
-
 
 
 # 爬取資料
@@ -18,11 +21,11 @@ def real_estate_crawler(year, season):
     res = requests.get("https://plvr.land.moi.gov.tw//DownloadSeason?season="+str(year)+"S"+str(season)+"&type=zip&fileName=lvr_landcsv.zip")
     
     # save content to file
-    fname = 'data/'+str(year)+str(season)+'.zip'
+    fname = 'raw_data/'+str(year)+str(season)+'.zip'
     open(fname, 'wb').write(res.content)
 
     # make additional folder for files to extract
-    folder = 'data/real_estate' + str(year) + str(season)
+    folder = 'raw_data/real_estate' + str(year) + str(season)
     if not os.path.isdir(folder):
         os.mkdir(folder)
 
@@ -30,62 +33,38 @@ def real_estate_crawler(year, season):
     with zipfile.ZipFile(fname, 'r') as zip_ref:
         zip_ref.extractall(folder)
 
-'''
-單季下載
-real_estate_crawler(101, 3)
-'''
-
 # 批次下載
 start_y = int(input("輸入查詢起始年份：民國 "))
-end_y = int(input("輸入查詢結束年份：民國 ") + 1
-for year in range(start_y, end_y ):
+end_y = int(input("輸入查詢結束年份：民國 "))
+
+for year in range(start_y, end_y+1):
     for season in range(1,5):
         print(year, season)
         real_estate_crawler(year, season)
 
 
-
-
 # 歷年資料夾
 # 指定路徑
-path = "data" 
-# 解壓縮後的資料夾列表
+path = "raw_data" 
 
-
-
-# dirs = [資料夾名稱前四個字為 real 且 109 年的資料] 
 # 可改 for 迴圈批次處理個年分
-dirs = [d for d in os.listdir(path) if d[:4] == 'real' and 1100>int(d[-4:])> 1090 ]
-
-# print(len(dirs)) # 對照用
+start_yq = int(str(start_y)+"0")
+end_yq = int(str(end_y+1)+"0")
+dirs = [d for d in os.listdir(path) if d[:4] == 'real' and end_yq>int(d[-4:])> start_yq ]
 
 dfs = []
+
 alphs = "a b c d e f g h i j k m n o p q t u v w x z".upper().split()
 for d in dirs:
-    # print("前四個字為 real 的資料夾名稱:", d) # 檢查用
-    # 依序將各資料夾代入路徑，讀取 csv 檔
     for alph in alphs:
-        df = pd.read_csv(os.path.join('data', d,f'{alph}_lvr_land_a.csv'))
-        df['Q'] = d[-1] # 新增「季」欄位，辨識查找用
+        path = os.path.join('raw_data', d,f'{alph}_lvr_land_a.csv')
+        print("Reading:", path)
+        df = pd.read_csv(path)
         dfs.append(df.iloc[1:])
 
-'''
-path = "data" 
-dirs = [d for d in os.listdir(path) if d[:4] == 'real' and int(d[-4:])>100] # dirs = [資料夾名稱前四個字為 real 且 10601 之後的資料] 
-dfs = []
-for d in dirs:
-    df = pd.read_csv(os.path.join('data', d,'a_lvr_land_a.csv'))
-    df['Q'] = d[-1] # 新增「季」欄位
-    dfs.append(df.iloc[1:])
-'''
 
 df = pd.concat(dfs)
-print(df.iloc[1])
-df1 = df.duplicated()
-print(df1.index(True))
+print("\n第一列資料：\n", df.iloc[1])
+print(df.info())
 
-# 觀察資料
-# want_know_list = df[["編號", "鄉鎮市區", "土地區段位置建物區段門牌"]]
-# print(want_know_list.info()) 
-
-df.to_csv("all_109.csv", encoding="utf-8-sig")
+df.to_csv(f"raw_data\\all_{start_y}_to_{end_y}.csv", encoding="utf-8-sig")
